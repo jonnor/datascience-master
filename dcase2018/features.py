cea90e2f9358cbd7dbf286197538cddc82f3daae
Jon Nordby <jononor@gmail.com>
2018-06-18 12:44:50 +0200

    dcase2018: Initial feature extraction script

---
+++ dcase2018/features.py
@@ -0,0 +1,51 @@
+
+import os
+import sys
+
+import pandas
+import numpy
+
+import librosa 
+
+def calculate_mel(inpath):
+    y, sr = librosa.load(inpath, offset=0)
+    mel = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+    return mel
+
+def extract_mel(inpath):
+    out = inpath.replace('.wav', '.mel.csv')
+    if os.path.exists(out):
+        return None
+
+    mel = calculate_mel(inpath)
+
+    df = pandas.DataFrame(mel.T)
+    df.to_csv(out, index=False)
+    return mel
+
+def extract_mfcc(inpath, mel):
+    out = inpath.replace('.wav', '.mfcc.csv')
+    if os.path.exists(out):
+        return None
+
+    if mel is None:
+        mel = calculate_mel(inpath)
+    mfcc = librosa.feature.mfcc(S=mel, n_fft=2048, hop_length=512, n_mfcc=20)
+
+    df = pandas.DataFrame(mfcc.T)
+    df.to_csv(out, index=False)
+    return mfcc
+
+def extract_features(wavpath):
+    m = extract_mel(wavpath)
+    f = extract_mfcc(wavpath, m)
+
+def main():
+    files = sys.argv[1:]
+    total = len(files)
+    for i, f in enumerate(files):
+        print('{}/{}: {}'.format(i, total, f))
+        extract_features(f)
+
+if __name__ == '__main__':
+    main()

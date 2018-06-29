f22d13c014959a8ac50b32419b8461123111bee6
Jon Nordby <jononor@gmail.com>
2018-06-29 02:00:16 +0200

    dcase2018: Test Goertzel filter for spectrogram
    Seems to work.
    Better than STFT/melspectrogram from librosa when applying mean normalization??

--- dcase2018/detectbirdsmodule.cpp
+++ dcase2018/detectbirdsmodule.cpp
@@ -5,39 +5,44 @@
 #include <pybind11/stl.h>
 #include <pybind11/numpy.h>
 
-float goertzel(int numSamples,int TARGET_FREQUENCY,int SAMPLING_RATE, float* data)
+// TODO: speedup by precalculating coefficients: sine,cosine,
+float goertzel(float* data, int n_samples, int frequency, int sr)
 {
-    int     k,i;
-    float   floatnumSamples;
-    float   omega,sine,cosine,coeff,q0,q1,q2,result,real,imag;
+    const float scale = n_samples / 2.0;
 
-    floatnumSamples = (float) numSamples;
-    k = (int) (0.5 + ((floatnumSamples * TARGET_FREQUENCY) / SAMPLING_RATE));
-    omega = (2.0 * M_PI * k) / floatnumSamples;
-    sine = sin(omega);
-    cosine = cos(omega);
-    coeff = 2.0 * cosine;
-    q0=0;
-    q1=0;
-    q2=0;
+    float samples = (float) n_samples;
+    float k = (int) (0.5 + ((samples * frequency) / sr));
+    float omega = (2.0 * M_PI * k) / samples;
+    float sine = sin(omega);
+    float cosine = cos(omega);
 
-    for(i=0; i<numSamples; i++)
-    {
+    float coeff = 2.0 * cosine;
+    float q0=0;
+    float q1=0;
+    float q2=0;
+
+    for(int i=0; i<n_samples; i++) {
         q0 = coeff * q1 - q2 + data[i];
         q2 = q1;
         q1 = q0;
     }
-    real = (q1 - q2 * cosine);
-    imag = (q2 * sine);
-    result = sqrtf(real*real + imag*imag);
-    return result;
+ 
+    const float real = (q1 - q2 * cosine) / scale;
+    const float imag = (q2 * sine) / scale;
+
+    float magnitude = sqrtf(real*real + imag*imag);
+    return magnitude;
+}
+
+float goertzel_py(std::vector<float> samples, int frequency, int sr) {
+    return goertzel(&samples[0], samples.size(), frequency, sr);
 }
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(emtreesc, m) {
+PYBIND11_MODULE(detectbirds, m) {
     m.doc() = "Detect birdsong in audio";
 
-    m.def("goertzel", goertzel);
+    m.def("goertzel", goertzel_py);
 }
 

d451f3979df4048ec98eccc3cace70c222c058ec
Jon Nordby <jononor@gmail.com>
2018-06-30 01:59:49 +0200

    dcase2018: Move Goertzel out of Python module

--- dcase2018/detectbirds.h
+++ dcase2018/detectbirds.h
@@ -13,6 +13,45 @@ emtrees_predict(EmtreesModel *m, float *f, int32_t l) {
 }
 EmtreesModel *birddetect_model = NULL;
 
+
+typedef struct _EmGoertzel {
+    float sine;
+    float cosine;
+} EmGoertzel;
+
+EmGoertzel
+emgoertzel_coefficients(int frequency, int n_samples, int sr)
+{
+    const float samples = (float)n_samples;
+    const float k = (int)(0.5 + ((samples * frequency) / sr));
+    const float omega = (2.0 * M_PI * k) / samples;
+    const EmGoertzel c  = { sin(omega), cos(omega) }; 
+    return c;
+}
+
+
+float
+emgoertzel_run(EmGoertzel c, const float* data, int n_samples)
+{
+    const float coeff = 2.0 * c.cosine;
+    float q0=0;
+    float q1=0;
+    float q2=0;
+
+    for(int i=0; i<n_samples; i++) {
+        q0 = coeff * q1 - q2 + data[i];
+        q2 = q1;
+        q1 = q0;
+    }
+ 
+    const float scale = n_samples / 2.0;
+    const float real = (q1 - q2 * c.cosine) / scale;
+    const float imag = (q2 * c.sine) / scale;
+
+    const float magnitude = sqrtf(real*real + imag*imag);
+    return magnitude;
+}
+
 // emaudio.h
 typedef struct {
     float *data;

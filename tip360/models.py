1a64450060068e240385debcfa718902464446bb
Jon Nordby <jononor@gmail.com>
2018-12-04 17:14:42 +0100

    tip360: Use z3 in model

--- tip360/models.py
+++ tip360/models.py
@@ -49,12 +49,99 @@ def transmit_costs(bytes, pricebreaks):
     pass
 
 
+def octave_bands_ram(window, samples_second=8, n_bands=31, precision=2):
+    samples = window * samples_second
+    bytes = samples * n_bands * precision 
+    return bytes
+
+def radio_time(bytes, rate=50000, setup_time=10):
+    transmit_time = bytes / rate
+    return setup_time + transmit_time
+
+# Activites
+#  mic-read
+#  noise-calculate
+#  noiseid-calculate
+#  transmit
+#
+# Aspects
+#  RAM use
+#  energy use (cpu usage, data produced)
+
 # TODO: model RAM needed
 # TODO: model temp storage needed between transmission intervals
 # TODO: model data needs for noise measurement
-# TODO: allow hybrid mixture of raw,features,classification-summary
+# TODO: model CPU capacity
+def noise_sensor(ram_kb=40,
+        transmit_period_hours = 24,
+        noiseid_window=60,
+        battery_selfdischarge=0.05,
+        battery_days=365,
+        battery_mah=3000):
 
-def main():
+    import z3
+
+    cpu_ram = z3.Int('cpu_ram')
+    energy_budget = z3.Int('energy_budget') # microamps/day
+
+    # take into account self-discharge
+    battery_mah = battery_mah * (1.0-battery_selfdischarge)
+
+    battery_uas = battery_mah * 1000 * 3600
+    energy_day = battery_uas / battery_days
+
+    # FIXME: calculate from activities
+    transmit_size = 2048 + 30000
+    transmit_time = radio_time(transmit_size)
+    
+    transmit_period = transmit_period_hours / 24*60*60
+
+    ram_use = [
+        z3.IntVal(256*8), # audio buffers
+        z3.IntVal(2048), # transmit payload. TODO: function of transmit_interval
+        z3.IntVal(octave_bands_ram(noiseid_window)),
+    ]
+    
+    cpu_sleep_a = 10e-6
+    cpu_run_a = 1e-3
+    modem_a = 50e-3
+
+    def energy(amp, seconds=1.0, period=1.0):
+        ua = cpu_run_a*1e6
+        dutycycle = seconds / period
+        return ua * dutycycle
+
+    energy_use = [
+        z3.RealVal(energy(cpu_sleep_a)),
+        z3.RealVal(energy(cpu_run_a, 10e-6, 1/1000)), # audio read
+        z3.RealVal(energy(cpu_run_a, 100e-6, 1/1000)), # noise calc
+        z3.RealVal(energy(modem_a, transmit_time, transmit_period)), # transmit
+    ]
+
+    constraints = [
+        cpu_ram == ram_kb*1000,
+        z3.Sum(ram_use) < cpu_ram,
+        energy_budget == energy_day,
+        z3.Sum(energy_use) < energy_budget,
+    ] 
+
+    s = z3.Solver()
+    for c in constraints:
+        s.add(c)
+    return s
+
+def test_pass():
+    m = noise_sensor()
+    s = m.check()
+    assert str(s) == 'sat', m
+
+def test_fails():
+    m = noise_sensor(ram_kb=1)
+    s = m.check()
+    assert str(s) == 'unsat', m
+
+
+def temp():
     frame_shift = 512/44100
 
     audio = audio_size(44100, bits=16)
@@ -90,6 +177,11 @@ def main():
     print('days', days)
 
     print(locals())
+
+
+def main():
+    noise_sensor()
+ 
 
 if __name__ == '__main__':
     main()
